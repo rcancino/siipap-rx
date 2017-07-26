36ac2506f7cc6d124c34ff62dd7f65b4e4311f65
Ruben Cancino <rubencancino6@gmail.com>
2017-07-26 14:55:10 -0500

    fix compra controller

--- sx-server/grails-app/controllers/sx/compras/CompraController.groovy
+++ sx-server/grails-app/controllers/sx/compras/CompraController.groovy
@@ -16,9 +16,8 @@ class CompraController extends RestfulController{
         super(Compra)
     }
 
-     @Override
+    @Override
     protected List listAllResources(Map params) {
-        params.max=500
         def query = Compra.where {}
         
         if(params.pendientes){
@@ -27,29 +26,15 @@ class CompraController extends RestfulController{
         return query.list(params)
     }
 
-    @Transactional
-    def save(Compra compraInstance) {
-        compraInstance.folio = Folio.nextFolio('COMPRA',ser)
-        compraInstance.validate()
-        if (compraInstance.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond compraInstance.errors, view:'create' // STATUS CODE 422
-            return
-        }
-
-        saveResource compraInstance
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [classMessageArg, compraInstance.id])
-                redirect compraInstance
-            }
-            '*' {
-                response.addHeader(HttpHeaders.LOCATION,
-                        grailsLinkGenerator.link( resource: this.controllerName, action: 'show',id: compraInstance.id, absolute: true,
-                                namespace: hasProperty('namespace') ? this.namespace : null ))
-                respond compraInstance, [status: CREATED, view:'show']
-            }
-        }
+    @Override
+    protected Integer countResources() {
+        return super.countResources()
     }
+
+    @Override
+    protected Object saveResource(Compra compraInstance) {
+        compraInstance.folio = Folio.nextFolio('COMPRA',ser)
+        compraInstance.save flush: true
+    }
+
 }
